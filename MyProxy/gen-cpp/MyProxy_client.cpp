ef9b88d08995d681d4bb18687cef45015c59f62e
kszr <abhishek.rob@gmail.com>
2016-03-30 23:17:46 -0400

    Implementing url list

--- MyProxy/gen-cpp/MyProxy_client.cpp
+++ MyProxy/gen-cpp/MyProxy_client.cpp
@@ -5,6 +5,7 @@
 #include <protocol/TBinaryProtocol.h>
 
 #include <iostream>
+#include <fstream> 
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
@@ -12,9 +13,9 @@ using namespace apache::thrift::transport;
 using namespace MyProxy;
 using namespace  std;
 
-static void load_file_list(string filepath);
+static void load_url_list(string filepath);
 
-static vector<string> filelist;
+static vector<string> urllist;
 
 int main(int argc, char **argv) {
     boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
@@ -25,15 +26,27 @@ int main(int argc, char **argv) {
     transport->open();
     client.ping();
     string webcontent;
-    string url= "www.google.com";
-    client.httpget_1(webcontent, url);
+    //string url= "www.google.com";
+    load_url_list("../workloads/lfu_beats_lru.txt");
+    for(auto& str: urllist) {
+        //client.httpget_1(webcontent, str);
+    }
     //cout<<"The web page content for URL: "<<url<<" is:" <<webcontent<<endl;
     transport->close();
 
     return 0;
 }
 
-static void load_file_list(string filepath) {
-    
+/**
+ * Loads a list of URLs from a file.
+ */
+static void load_url_list(const string filepath) {
+    ifstream file(filepath.c_str());
+    string str; 
+    while (std::getline(file, str))
+    {
+        urllist.push_back(str);
+        cout << str << endl;
+    }
 }
 
