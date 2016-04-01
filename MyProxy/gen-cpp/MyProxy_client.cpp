1c478ed3c32e5e0977deccdfae92fd90722d341c
kszr <abhishek.rob@gmail.com>
2016-04-01 01:53:37 -0400

    changes

--- MyProxy/gen-cpp/MyProxy_client.cpp
+++ MyProxy/gen-cpp/MyProxy_client.cpp
@@ -29,8 +29,10 @@ int main(int argc, char **argv) {
     //string url= "www.google.com";
     //load_url_list("../workloads/rnd_beats_lru.txt");
     Workload workload = Workload();
-    workload.generate_lru_good_workload();
-    urllist = workload.get_lru_good_workload();
+    
+    /* Specify workload below */
+    workload.generate_lru_bad_workload();
+    urllist = workload.get_lru_bad_workload();
     
     for(auto& str : urllist) {
         client.httpget_1(webcontent, str);
